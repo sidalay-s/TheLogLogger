56b1f710b268a7b25a53ee01d4a0ed4735615b87
Sid Shingaki <sidalaytv@gmail.com>
2022-09-10 13:05:44 -0700

    chore: (WIP) sql class
    - added new functions to SQL class

--- include/sql.hpp
+++ include/sql.hpp
@@ -2,15 +2,22 @@
 #define SQL_HPP
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include "sqlite3.h"
 
-enum class Command {Create, Read, Update, Delete};
+enum class Command {CREATE, READ, UPDATE, DELETE, INSERT};
 
 class SQL
 {
 public:
+    void Menu();
+    void Create();
+    void Read();
+    void Update();
+    void Delete();
+
     void CreateDB();
     void CreateTable();
     void DeleteTable(const std::string TableName);
@@ -18,16 +25,18 @@ public:
     void UpdateData();
     void DeleteData(const std::string DataID);
     void SelectData();
+    bool GetExit() {return Exit;}
 private:
-    const std::string& Directory{"Journal.db"};
+    const std::string Directory{"Journal.db"};
     sqlite3* Database{nullptr};
     char* MessageError{nullptr};
+    bool Exit{false};
 
     void Execute(std::string SqlCode, Command Cmd);
     void CheckExecute(int Exe, Command Cmd);
     friend int Callback(void* NotUsed, int Argc, char** Argv, char** azColName);
 public:
-    SQL();
+    SQL() = default;
 };
 
 #endif // SQL_HPP
--- src/sql.cpp
+++ src/sql.cpp
@@ -1,38 +1,134 @@
 #include "sql.hpp"
 
+int Callback(void* NotUsed, int Argc, char** Argv, char** azColName)
+{
+    for (int i = 0; i < Argc; ++i)
+    {
+        std::cout << azColName[i] << ": " << Argv[i] << "\n";
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
+
+void SQL::Menu()
+{
+
+}
+
+void SQL::Create()
+{
+
+}
+
+void SQL::Read()
+{
+
+}
+
+void SQL::Update()
+{
+
+}
+
+void SQL::Delete()
+{
+
+}
+
 void SQL::CreateDB()
 {
     sqlite3_open(Directory.c_str(), &Database);
     sqlite3_close(Database);
+    std::cout << "The Log Logger\n\n";
 }
 
 void SQL::CreateTable()
 {
     sqlite3_open(Directory.c_str(), &Database);
+
+    std::string TableMeal{
+        "CREATE TABLE IF NOT EXISTS Meal("
+        "ID                 INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "Date               TEXT    NOT NULL, "
+        "Meal               TEXT    NOT NULL, " 
+        "MealName           TEXT    NOT NULL, "
+        "Ingredients        TEXT    NOT NULL, "
+        "Description        TEXT    NOT NULL); "
+    };
+    Execute(TableMeal, Command::CREATE);
+
+    std::string TableBowelMovement{
+        "CREATE TABLE IF NOT EXISTS Poop("
+        "ID                 INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "Date               TEXT    NOT NULL, "
+        "BowelMovement      TEXT    NOT NULL, " 
+        "Description        TEXT    NOT NULL); "
+    };
+    Execute(TableBowelMovement, Command::CREATE);
+
     sqlite3_close(Database);
 }
 
 void SQL::DeleteTable(const std::string TableName)
 {
     sqlite3_open(Directory.c_str(), &Database);
+    
+    std::string SqlCode{"DROP TABLE IF EXISTS "};
+    SqlCode.append(TableName).append(";");
+
+    Execute(SqlCode, Command::DELETE);
+
     sqlite3_close(Database);
 }
 
 void SQL::InsertData()
 {
     sqlite3_open(Directory.c_str(), &Database);
+
+    std::string SqlCode{
+        "INSERT INTO Meal (Date, MealNumber, MealName, Ingredients, Description)"
+        "VALUES( "
+        "date(\"now\"), " 
+        "1, "
+        "\"Oatmeal\", "
+        "\"\n- Rolled Oats\n"
+        "- Apples\n"
+        "- Flax Seed\n"
+        "- Cinnamon\n"
+        "- Almond Milk\n"
+        "- MCT Oil\n"
+        "- Maple Syrup\n"
+        "- Vanilla Extract\n"
+        "- Cinnamon Roll bits\", "
+        "\"Stomach Bloat\"); "
+    };
+
+    Execute(SqlCode, Command::INSERT);
+
     sqlite3_close(Database);
 }
 
 void SQL::UpdateData()
 {
     sqlite3_open(Directory.c_str(), &Database);
+
+    std::string SqlCode{"UPDATE ENTRY SET ID = 2 WHERE ID = 5"};
+
+    Execute(SqlCode, Command::UPDATE);
+
     sqlite3_close(Database);
 }
 
 void SQL::DeleteData(const std::string DataID)
 {
     sqlite3_open(Directory.c_str(), &Database);
+
+    std::string SqlCode{"DELETE FROM ENTRY WHERE ID = "};
+    SqlCode.append(DataID).append(";");
+
+    Execute(SqlCode, Command::DELETE);
+
     sqlite3_close(Database);
 }
 
@@ -42,7 +138,7 @@ void SQL::SelectData()
     
     std::string SqlCode{};
 
-    Execute(SqlCode, Command::Read);
+    Execute(SqlCode, Command::READ);
 
     sqlite3_close(Database);
 }
@@ -53,7 +149,7 @@ void SQL::Execute(std::string SqlCode, Command Cmd)
 
     switch (Cmd)
     {
-        case Command::Read:
+        case Command::READ:
             Exe = sqlite3_exec(Database, SqlCode.c_str(), Callback, NULL, NULL);
             break;
         default:
@@ -70,17 +166,20 @@ void SQL::CheckExecute(int Exe, Command Cmd)
     {
         switch (Cmd)
         {
-            case Command::Create:
+            case Command::CREATE:
                 std::cerr << "Error on Create\n" << std::endl;
                 break;
-            case Command::Read:
+            case Command::READ:
                 std::cerr << "Error on Select\n" << std::endl;
                 break;
-            case Command::Update:
+            case Command::UPDATE:
                 std::cerr << "Error on Update\n" << std::endl;
                 break;
-            case Command::Delete:
+            case Command::DELETE:
                 std::cerr << "Error on Delete\n" << std::endl;
+                break;
+            case Command::INSERT:
+                std::cerr << "Error on Insert\n" << std::endl;
                 break;
         }
         sqlite3_free(MessageError);
@@ -89,29 +188,21 @@ void SQL::CheckExecute(int Exe, Command Cmd)
     {
         switch (Cmd)
         {
-            case Command::Create:
+            case Command::CREATE:
                 std::cout << "Table created sucessfully\n" << std::endl;
                 break;
-            case Command::Read:
+            case Command::READ:
                 std::cout << "Data Selected sucessfully\n" << std::endl;
                 break;
-            case Command::Update:
+            case Command::UPDATE:
                 std::cout << "Data Updated sucessfully\n" << std::endl;
                 break;
-            case Command::Delete:
+            case Command::DELETE:
                 std::cout << "Data Deleted sucessfully\n" << std::endl;
+                break;
+            case Command::INSERT:
+                std::cout << "Data Inserted succesfully\n" << std::endl;
                 break;
         }
     }
-}
-
-int Callback(void* NotUsed, int Argc, char** Argv, char** azColName)
-{
-    for (int i = 0; i < Argc; ++i)
-    {
-        std::cout << azColName[i] << ": " << Argv[i] << "\n";
-    }
-
-    std::cout << std::endl;
-    return 0;
 }
